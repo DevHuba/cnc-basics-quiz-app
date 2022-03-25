e56b8fa1de91220c01c98bbe78058e72db86302b
DevHuba <devhuba@gmail.com>
2022-03-25 14:48:24 +0200

    add scalable bg images, finish first version of home screen, add quiz activity

--- app/src/main/java/com/example/cncbasicsquizapp/MainActivity.kt
+++ app/src/main/java/com/example/cncbasicsquizapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.cncbasicsquizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.cncbasicsquizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val btnStart : Button = binding.btnStart
+        val etName : EditText = binding.etName
+
+
+        btnStart.setOnClickListener {
+            if (etName.text.isNotEmpty()) {
+
+            }
+            Toast.makeText(this,"Please enter your name",Toast.LENGTH_SHORT).show()
+
+        }
+
+
+
+
+
     }
 }
