3aa286d53d42aeac244aafe0dc2938231d3b69a7
DevHuba <devhuba@gmail.com>
2022-04-07 18:47:49 +0300

    fix of result screen bug

--- app/src/main/java/com/example/cncbasicsquizapp/Constants.kt
+++ app/src/main/java/com/example/cncbasicsquizapp/Constants.kt
@@ -434,9 +434,9 @@ object Constants {
         Collections.addAll(
             questionsList, question1,
             question2, question3, question4, question5,
-            question6, question7, question8,question9,question10,
-            question11,question12,question13,question14,question15,
-            question16,question17, question18,question19,question20,
+//            question6, question7, question8,question9,question10,
+//            question11,question12,question13,question14,question15,
+//            question16,question17, question18,question19,question20,
 //            question21,question22,question23,question24,question25,
 //            question26,question27,question28,question29,question30
 
